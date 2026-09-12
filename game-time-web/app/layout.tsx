bb05ea69e7bbdfa3a80a0e99c6cbb74dbad308a5
Aaron Villalobos <aaron.villalobos4@gmail.com>
2026-09-12 12:54:12 -0500

    Added Google Tag Manager Script

--- game-time-web/app/layout.tsx
+++ game-time-web/app/layout.tsx
@@ -15,6 +15,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K2JMPS5Q');`}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Google Tag (gtag.js) */}
         <Script
           strategy="afterInteractive"
